9b5bc95f313be8d28c80bb8ea3a38732363a0bee
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-19 17:34:49 +0100

    Add files via upload

---
+++ commands/serverinfo.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'serverinfo',
+    description: "Serverinfo",
+    execute(message, args, Discord) {
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#FFD7E4')
+        .setTitle('Serverinfo')
+        .setDescription('All information of the server.')
+        .addFields(
+            {name: 'Server name', value: '${message.guild.name}'},
+            {name: 'Total members', value: '${message.guild.memberCount}'},
+            {name: 'Owner', value: '${message.guild.owner}'},
+            {name: 'Region', value: '${message.guild.region}'}
+        )
+        .setFooter('Bot created by: Bencee#7253');
+
+        message.channel.send(newEmbed);
+    }
+}
