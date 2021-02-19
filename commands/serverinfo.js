72abadbf11c2d7a2a892e0bd4587db3ee1e99f43
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-19 17:38:59 +0100

    Add files via upload

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,10 +7,10 @@ module.exports = {
         .setTitle('Serverinfo')
         .setDescription('All information of the server.')
         .addFields(
-            {name: 'Server name', value: '${message.guild.name}'},
-            {name: 'Total members', value: '${message.guild.memberCount}'},
-            {name: 'Owner', value: '${message.guild.owner}'},
-            {name: 'Region', value: '${message.guild.region}'}
+            {name: 'Server name', value: `${message.guild.name}`},
+            {name: 'Total members', value: `${message.guild.memberCount}`},
+            {name: 'Owner', value: `${message.guild.owner}`},
+            {name: 'Region', value: `${message.guild.region}`}
         )
         .setFooter('Bot created by: Bencee#7253');
 
