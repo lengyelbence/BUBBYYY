ed26d1becf06c82d15b8c74d8eacf2ce5e0465df
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-19 11:34:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log('Bubby is online!');
+});
+
+client.on('message', message =>{
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'help'){
+        client.commands.get('help').execute(message, args, Discord);
+    } else if (command === 'play') {
+        client.commands.get('play').execute(message, args);
+    } else if (command === 'leave') {
+        client.commands.get('leave').execute(message, args);
+    }
+   
+});
+
+client.login(process.env.BOT_TOKEN);
