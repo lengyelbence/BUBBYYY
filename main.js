d64a3b18cb36ecd92b945c8b922b1abdee50d0d5
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-19 17:19:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ client.on('message', message =>{
     } else if (command === 'leave') {
         client.commands.get('leave').execute(message, args);
     } else if (message.content === `serverinfo`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nOwner: ${message.guild.owner}`);
     }
 
    
