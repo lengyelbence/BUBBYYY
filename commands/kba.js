fb36b2001cfeaa0657f34eb7e57b1ba606cb348a
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-19 22:27:02 +0100

    Add files via upload

--- commands/kba.js
+++ commands/kba.js
@@ -12,7 +12,6 @@ module.exports = {
             {name: 'Online', value: `${client.guild.cache.size}`},
             {name: 'Prefix', value: 'b!'},
             {name: 'Help', value: 'b!help'},
-            {name: 'Support server', value: 'https://discord.gg/Rgcfqdkbxn'},
         )
         .setFooter('Bot created by: Bencee#7253');
 
