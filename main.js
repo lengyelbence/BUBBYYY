bc99d1829125e7cd18c27547372853db5f0ddb9e
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-20 14:16:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ client.on('message', message =>{
 		return message.channel.send(`***Bubby online on ${client.guilds.cache.size} servers***`);
 	} 
       // If the message content starts with "!kick"
-    if (message.content.startsWith('!kick')) {
+    if (message.content.startsWith('b!kick')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
