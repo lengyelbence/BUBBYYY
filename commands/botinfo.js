c997bd223584a3ce3137339b7760c5e1210e5478
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-02-20 13:12:07 +0100

    Add files via upload

---
+++ commands/botinfo.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'botinfo',
+    description: "Botinfo",
+    execute(message, args, Discord) {
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#FFD7E4')
+        .setTitle('Botinfo')
+        .setDescription('Everything you need to know about Bubby.')
+        .addFields(
+            {name: 'Owner', value: '@Bencee#7253'},
+            {name: 'Help', value: 'Type: b!help'},
+            {name: 'Prefix', value: 'b!'},
+            {name: 'Ping', value: `${Date.now() - message.createdTimestamp}ms`}
+        )
+        .setFooter('Bot created by: Bencee#7253');
+
+        message.channel.send(newEmbed);
+    }
+}
