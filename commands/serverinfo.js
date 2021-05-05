5c97e7a66976e13afed35c230d0e52509fb3fbe9
Lengyel Bence <36980075+lengyelbence@users.noreply.github.com>
2021-05-05 19:48:31 +0200

    Add files via upload

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,14 +6,12 @@ module.exports = {
         .setColor('#FFD7E4')
         .setTitle('╔═══════ Serverinfo ═══════╗')
         .setDescription('All information of the server.')
-        .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-        .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
-        .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
-        .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
-        .addField('Location', message.guild.region, true)
-        .addField('Created', message.guild.createdAt.toLocaleString(), true)
-        .addBlankField(true)
-        .setTimestamp()
+        .addFields(
+            {name: 'Server name', value: `${message.guild.name}`},
+            {name: 'Total members', value: `${message.guild.memberCount}`},
+            {name: 'Owner', value: `${message.guild.owner}`},
+            {name: 'Region', value: `${message.guild.region}`}
+        )
         .setFooter('Bot created by: Bencee#7253');
 
         message.channel.send(newEmbed);
